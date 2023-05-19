97ba011b3ccd6440bfdaf72893dfb594e9dfba84
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-05-19 12:58:05 +0900

    Setting : firebase 세팅

---
+++ src/Firebase/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAJHqvuR5G1A8ed67PvaWX2bUUvjC76p44",
+  authDomain: "login-ab753.firebaseapp.com",
+  projectId: "login-ab753",
+  storageBucket: "login-ab753.appspot.com",
+  messagingSenderId: "851578899769",
+  appId: "1:851578899769:web:27b59d25c708e3b68f2ce8",
+  measurementId: "G-EFSWE9J3R2"
+};
